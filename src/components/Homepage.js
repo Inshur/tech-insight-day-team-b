77ec64683504b846a23635962148fe3068693a1b
George Smith <georgelukesmith@outlook.com>
2024-01-24 14:20:01 +0000

    test commit

---
+++ src/components/Homepage.js
@@ -0,0 +1,14 @@
+import { React } from 'react';
+
+const Homepage = () => {
+  return (
+    <div>
+        <h1>DSUI</h1>
+        <div>
+          <input name="searchBar" type="search" />
+        </div>
+    </div>
+  );
+};
+
+export default Homepage;
